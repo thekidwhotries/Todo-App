6f82f5a9dfd9d986448627c988064d7388e6040a
Emilio <Miotalia1@gmail.com>
2025-04-18 16:50:40 -0700

    added changes in code

--- src/pages/Untitled-1.js
+++ src/pages/Untitled-1.js
@@ -20,3 +20,34 @@ const todos = [
 // map
 
 // for loop 
+
+
+
+
+
+// Create a function that takes 2 input's i.e num1 and num2 
+// and it should return the the sum of these 2 number's
+
+// const addNumbers(num1, num2) =>{
+// return num1+num2
+// }
+
+
+// create a function which will take 2 number and divide them and return the result 
+
+// const divideNumbers (num1, num2) => {
+//     return num1/num2
+// }
+
+// create a function to add data in local d
+
+const addNumbers (num1, num2) => {
+localStorage.setItem (JSON.stringify( num1+num2))
+}
+
+
+
+
+
+
+
